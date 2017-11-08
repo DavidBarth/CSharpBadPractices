1cc289e33fc3eb3d83912665c81ec58930994a32
David Barth <davidbarth80@gmail.com>
2017-11-08 19:47:30 +0000

    Add replace magic numbers and refactor naming

---
+++ CSharpBadPractices/AccountStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CSharpBadPractices
+{
+    public enum AccountStatus
+    {
+        NotRegistered = 1,
+        SimpleCustomer = 2,
+        ValuableCustomer = 3,
+        MostValuableCustomer = 4
+    }
+}
--- CSharpBadPractices/Class1.cs
+++ CSharpBadPractices/Class1.cs
@@ -1,29 +1,29 @@
 ﻿using System;
 namespace CSharpBadPractices
 {
-    public class Class1
+    public class DiscountManager
     {
-        public decimal Calculate(decimal amount, int type, int years)
+        public decimal ApplyDiscount(decimal price, AccountStatus accountStatus, int timeOfHavingAccountInYears)
         {
-            decimal result = 0;
-            decimal disc = (years > 5) ? (decimal)5 / 100 : (decimal)years / 100;
-            if (type == 1)
+            decimal priceAfterDiscountApplied = 0;
+            decimal discountForLoyaltyInPercentage = (timeOfHavingAccountInYears > 5) ? (decimal)5 / 100 : (decimal)timeOfHavingAccountInYears / 100;
+            if (accountStatus == AccountStatus.NotRegistered)
             {
-                result = amount;
+                priceAfterDiscountApplied = price;
             }
-            else if (type == 2)
+            else if (accountStatus == AccountStatus.SimpleCustomer)
             {
-                result = (amount - (0.1m * amount)) - disc * (amount - (0.1m * amount));
+                priceAfterDiscountApplied = (price - (0.1m * price)) - discountForLoyaltyInPercentage * (price - (0.1m * price));
             }
-            else if (type == 3)
+            else if (accountStatus == AccountStatus.ValuableCustomer
             {
-                result = (0.7m * amount) - disc * (0.7m * amount);
+                priceAfterDiscountApplied = (0.7m * price) - discountForLoyaltyInPercentage * (0.7m * price);
             }
-            else if (type == 4)
+                     else if (accountStatus == AccountStatus.MostValuableCustomer)
             {
-                result = (amount - (0.5m * amount)) - disc * (amount - (0.5m * amount));
+                priceAfterDiscountApplied = (price - (0.5m * price)) - discountForLoyaltyInPercentage * (price - (0.5m * price));
             }
-            return result;
+            return priceAfterDiscountApplied;
         }
     }
 }
